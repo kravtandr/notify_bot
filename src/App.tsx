f97af2ab7c17481d87a74c1235807b8b62542b3a
Андрей Кравцов <kan19u508@student.bmstu.ru>
2024-05-08 15:03:22 +0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -5,29 +5,47 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 import WebApp from '@twa-dev/sdk'
+import ImageUpload from './components/imageInput'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://ton.org/dev" target="_blank">
-          <img src={twaLogo} className="logo" alt="TWA logo" />
-        </a>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>TWA + Vite + React</h1>
+      <h1>Notify</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Всего адресатов {count}
         </button>
       </div>
+      <label>
+        ChatId: <input type="checkbox" name="myCheckbox" />
+        <input name="InputChatId"/>
+        <br />
+      </label>
+      <label>
+        Имя содержит: <input type="checkbox" name="myCheckbox" /><input name="InputName" />
+        <br />
+      </label>
+      <label>
+        Write your post:
+        <br />
+        <textarea name="postContent" rows={4} cols={40} />
+        <br />
+      </label>
+      <ImageUpload />
+
+      
+      <select name="selectedFruit">
+        <option value="apple">Apple</option>
+        <option value="banana">Banana</option>
+        <option value="orange">Orange</option>
+      </select>
+      <progress value={0.5} />
+      <select>
+        <option value="someOption">Some option</option>
+        <option value="otherOption">Other option</option>
+      </select>
       {/*  */}
       <div className="card">
         <button onClick={() => WebApp.showAlert(`Hello World! Current count is ${count}`)}>
