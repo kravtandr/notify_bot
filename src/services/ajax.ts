cd6d2c969fff492b68e2fd5fd27ffd4c889026f3
Андрей Кравцов <kan19u508@student.bmstu.ru>
2024-05-11 16:15:13 +0300

    .

--- src/services/ajax.ts
+++ src/services/ajax.ts
@@ -1,6 +1,4 @@
-// import http from "../http-common";
-
-import axios from "axios";
+import http from "../http-common";
 
 const users = (file: File, onUploadProgress: any): Promise<any> => {
   let formData = new FormData();
@@ -20,7 +18,7 @@ const users = (file: File, onUploadProgress: any): Promise<any> => {
   formData.append("borninday", file);
   formData.append("borninmonth", file);
 
-  return axios.post("http://193.109.69.104:8080/upload", formData, {
+  return http.post("/upload", formData, {
     headers: {
       "Content-Type": "multipart/form-data",
     },
@@ -35,7 +33,7 @@ const notify = (file: File, onUploadProgress: any): Promise<any> => {
     formData.append("msg", "Test");
     formData.append("recipients", "398548347,398548347");
   
-    return axios.post("http://193.109.69.104:8080/notify", formData, {
+    return http.post("/notify", formData, {
       headers: {
         "Content-Type": "multipart/form-data",
       },
@@ -43,9 +41,14 @@ const notify = (file: File, onUploadProgress: any): Promise<any> => {
     });
   };
 
+const getFiles = () : Promise<any> => {
+  return http.get("/files");
+};
+
 const FileUploadService = {
   users,
   notify,
+  getFiles,
 };
 
 export default FileUploadService;
